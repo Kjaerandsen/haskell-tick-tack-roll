3e0155798a2705967992bd64e280d592f90d7939
jonakj <jonakj@stud.ntnu.no>
2022-03-14 19:45:28 +0100

    Added length checking function that checks if the length of the list is valid, also squaredInteger which returns an integer squared as a rounded integer

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,7 +7,7 @@ someFunc = putStrLn "someFunc"
 
 -- | roll left tests, should only allow grid sizes of 3x3 and larger odd numbers, else return empty arrays
 -- >>> rollLeft [3,2,1]
--- [3,2,1]
+-- []
 --
 -- >>> rollLeft [3,2,1,4]
 -- []
@@ -15,11 +15,6 @@ someFunc = putStrLn "someFunc"
 -- >>> rollLeft []
 -- []
 --
-
--- >>> sqrt 10
--- 3.1622776601683795
---
-
 
 -- | rollLeft function that handles rolling a board of 3 + 2x size to the left according to the game rules
 rollLeft :: [Int] -> [Int]
@@ -34,15 +29,55 @@ rollLeft x = do
 
 -- Need to check if the length sqrt is an integer or not.
 
+-- | lengthCheck tests, works only with valid grid sizes (3x3 + 2x) where x is a whole number
+-- if the length is less than 9, not squarable as a whole number, or not even 0 is returned.
+-- >>> lengthCheck [1,2,3,4,5,6,7,8,9]
+-- >>> lengthCheck[1,2,3,4,5,6,7,8,9,10,11]
+-- >>> lengthCheck[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+-- >>> lengthCheck []
+-- >>> lengthCheck [0,1,2]
+-- 3
+-- 0
+-- 0
+-- 0
+-- 0
+--
+
 -- | lengthCheck checks if the length is valid, returns the length if valid, 0 if not
+-- if the length is less than 9, not squarable as a whole number, or not even 0 is returned.
+-- else the length squared is returned
 lengthCheck :: [Int] -> Int
 lengthCheck x = do
     let len = length x
-    if len > 3 then
+    if len < 9 then
         0
     else if (len `mod` 2) /= 1 then
         0
-    else
+    else do
         -- Check if the length squared is a whole number
-        -- Return that number if true, else return 0
-        len
+        let rowLen = squaredInteger len
+        -- If not a whole number return 0
+        if (rowLen * rowLen) /= len then
+            0
+        -- Else return the row length
+        else
+            rowLen
+
+-- | Squared integer tests, works with positive, negative and zero values.
+-- >>> squaredInteger 0
+-- 0
+--
+-- >>> squaredInteger 2
+-- 1
+--
+-- >>> squaredInteger (-10)
+-- 0
+--
+
+-- | squaredInteger takes an integer and returns the integer squared rounded to an integer
+squaredInteger :: Int -> Int
+squaredInteger x = round . sqrt . fromIntegral $ x
+
+-- inspired by the first comment to the solution from kqr at stackoverflow:
+-- The comment: "How does it compare to intSqrt = floor . sqrt . fromInteger ? – kqr Nov 14, 2013 at 7:41"
+-- Link: https://stackoverflow.com/questions/19965149/integer-square-root-function-in-haskell
