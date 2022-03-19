4c0d07ca42b9f4f05bfe4278b0fa00d89a8749d8
jonakj <jonakj@stud.ntnu.no>
2022-03-19 15:34:59 +0100

    winCheck structure work

--- src/Lib.hs
+++ src/Lib.hs
@@ -40,4 +40,18 @@ printLine i = do
 -- | winCheck takes a grid, determines the grid size and player pieces, returns the winner
 -- or blank if no winner in the current board state
 winCheck :: [Char] -> Char
-winCheck [a] = '_'
+winCheck a = do
+    -- get the line length
+    let len = lengthCheck a
+    if len /= 0 then do
+        -- make a list of the left diagonal and check the item count of X and O
+
+        -- make a list of the right diagonal and check the item count of X and O
+
+        -- make a list of top down left, mid and right and check the counts of X and O
+
+        -- check line by line
+        '_'
+    else -- Default to blank output if invalid input
+        '_'
+    
