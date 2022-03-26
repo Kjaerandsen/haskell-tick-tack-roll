8610fd854c6cdf682a7c7efb7b057075a9336c46
jonakj <jonakj@stud.ntnu.no>
2022-03-26 13:17:59 +0100

    mainLoop has been changed to playerInput, uses placeholder input to perform a move and return a winner or empty char if no winner found

--- app/Main.hs
+++ app/Main.hs
@@ -4,15 +4,49 @@ import Roll -- Rolling functions
 import Lib
 
 main :: IO ()
-main = printBoard [1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5]
+main = do
+    printBoard [1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5]
+    let test = playerMove 'X' "_________"
+    putChar test
 
+-- Function for setting up the game, decide board size, mode (vs bot or player),
+-- if bot then which piece the bot plays as. Possibly also bot difficulty.
+
+-- | playerMove takes a player move, performs it and checks if a win condition occurrs
+-- Char which decides which piece the player plays
+-- Char array containing the game board
+playerMove :: Char -> [Char] -> Char
+playerMove playerPiece board = do
+    -- take user input
+    let move = 4
+    let rotate = "left"
+    -- parse it into the move let
+
+    -- Do the move here
+    let boardState = roll rotate (doMove playerPiece move board)
+
+    if boardState == [] then
+        '_'
+    else do
+        -- Check for a winner
+        let winner = winCheck board
+        if winner /= '_' then
+            -- Return the winner if there is one
+            winner
+        else if (membersInList '_' board) == 0 then
+            -- If there is no winner and all tiles are used up return a tie
+            '_'
+        else
+            ' '
 
 -- | doMove takes a move and a board, returns the board if the move is successfull
 -- else if the spot is occupied returns an empty array
 doMove :: Char -> Int -> [Char] -> [Char]
 doMove piece move board = do
-    if (length board) < move then
+    -- Check if the move is within the valid range for a move
+    if ((length board) < move) || (move < 1) then
         []
+    -- Check if the tile is occupied already
     else if board!!(move-1) /= '_' then
         []
     else
@@ -31,55 +65,3 @@ doMove piece move board = do
 -- >>> doMove 'X' 1 ['_','O','X','O','X','O','_','_','_']
 -- "XOXOXO___"
 --
-
--- >>> map ([]:) "XOXOXO___"
--- <interactive>:3701:12-22: error:
---     * Couldn't match type `Char' with `[[a]]'
---       Expected type: [[[a]]]
---         Actual type: [Char]
---     * In the second argument of `map', namely `"XOXOXO___"'
---       In the expression: map ([] :) "XOXOXO___"
---       In an equation for `it': it = map ([] :) "XOXOXO___"
---     * Relevant bindings include
---         it :: [[[a]]] (bound at <interactive>:3701:2)
---
-
--- Function for setting up the game, decide board size, mode (vs bot or player),
--- if bot then which piece the bot plays as. Possibly also bot difficulty.
-
-{-
--- | mainLoop takes a Bool which decides if the game is over or not
--- a Char which decides which piece the bot plays ['X','O','_'] if bot == '_' then the game is pvp
--- a bool which decides which players turn it is, True == 'X', False == 'O'
--- a Char array containing the game board
--- First bool might be redundant
-mainLoop :: Bool -> Char -> Char -> [Char] -> IO () -> Char
-mainLoop finished bot turn board = do
-    if finished then
-        '_'
-    else do
-        if turn == bot then // Let the bot do its turn
-            move = botTurn bot board
-        else do
-            // ask user for input
-            putStr "Player " + turn + "'s turn.\n Player input:"
-
-            move = the input
-
-        // Do the move here,
-        // check if it conflicts with a location on the board, then return a winner? do this within the function
-        let boardState = doMove move board
-        if boardState == [] then
-            '_'
-        else do
-            let winner = winCheck board
-            
-            if winner /= '_' then
-                winner
-            else do
-                if membersInList '_' board == 0 then
-                    '_'
-                else
-                    mainLoop finished bot turn board
--}
-
